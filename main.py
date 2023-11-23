631aba0ee837109bb870ddefd6b6d1451a310c59
Everly <66821749+everlyy@users.noreply.github.com>
2023-11-23 20:51:40 +0100

    Add types to variables and get rid of pyright warnings
    Unfortunately have to disable type checking on 3 lines because the
    pypresence library only accepts a string even though it works fine
    with None.

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ import traceback
 import time
 
 class LastFM_RPC:
-    def __init__(self, discord_client_id, lastfm_username, lastfm_api_key):
-        self._client_id = discord_client_id
-        self._username = lastfm_username
-        self._api_key = lastfm_api_key
-        self._now_playing = None
+    def __init__(self, discord_client_id: str, lastfm_username: str, lastfm_api_key: str):
+        self._client_id: str = discord_client_id
+        self._username: str = lastfm_username
+        self._api_key: str = lastfm_api_key
+        self._now_playing: pylast.Track | None = None
 
-        self._network = pylast.LastFMNetwork(api_key=self._api_key)
-        self._user = self._network.get_user(self._username)
+        self._network: pylast.LastFMNetwork = pylast.LastFMNetwork(api_key=self._api_key)
+        self._user: pylast.User = self._network.get_user(self._username)
 
-        self._rpc = pypresence.Presence(self._client_id)
+        self._rpc: pypresence.Presence = pypresence.Presence(self._client_id)
         self._rpc.connect()
 
     def update(self):
-        now_playing = self._user.get_now_playing()
+        now_playing: pylast.Track | None = self._user.get_now_playing()
 
         if self._now_playing == now_playing:
             return
@@ -27,27 +27,27 @@ class LastFM_RPC:
             self._rpc.clear()
             return
 
-        cover = None
+        cover: str | None = None
         if "image" in now_playing.info and len(now_playing.info["image"]) > 0:
                 cover = now_playing.info["image"][-1]
 
-        album = None
+        album: str | None = None
         if "album" in now_playing.info and now_playing.info["album"] is not None:
             album = now_playing.info["album"]
 
-        loved = now_playing.get_userloved()
+        loved: bool = now_playing.get_userloved() not in (None, False)
 
         print(f"Updating Discord RPC: {now_playing} | {album}")
         try:
             self._rpc.update(
                 details=str(now_playing.title),
                 state=f"By {now_playing.artist}",
-                large_image=cover,
-                large_text=album,
+                large_image=cover, # type: ignore
+                large_text=album,  # type: ignore
 
                 # Not showing the "loved" icon if there's no cover art because it'll show up
                 # really large, which is kinda ugly
-                small_image="loved" if loved and cover is not None else None,
+                small_image="loved" if loved and cover is not None else None, # type: ignore
                 small_text="Loved"
             )
         except pypresence.exceptions.PipeClosed:
@@ -57,7 +57,7 @@ class LastFM_RPC:
         self._now_playing = now_playing
 
 if __name__ == "__main__":
-    rpc = LastFM_RPC(
+    rpc: LastFM_RPC = LastFM_RPC(
         discord_client_id=DISCORD_CLIENT_ID,
         lastfm_username=LASTFM_USERNAME,
         lastfm_api_key=LASTFM_API_KEY
